157839aed270302e979f771e27655884d0667f0b
Bruno Cabral <cangotep2p@gmail.com>
2015-02-18 16:35:07 -0300

    Update xen-backup-vm.sh
    Remove pem need

--- xen-backup-vm.sh
+++ xen-backup-vm.sh
@@ -4,19 +4,18 @@ if [ "${#}" = "0" ]
 then
     echo "Backup virtual machine from remote XenServer over SSH"
     echo "usage:" 
-    echo "${0} [vm name] [ssh user host] [ssh pem key]"
+    echo "${0} [vm name] [ssh user host]"
     exit
 fi
 
 # $1 remote command
 remote_exec() {
     chmod 0600 $PEMKEY
-    ssh -i $PEMKEY -o "StrictHostKeyChecking no" $USERHOST $1
+    ssh -o "StrictHostKeyChecking no" $USERHOST $1
 }
 
 VMNAME=${1}
 USERHOST=${2}
-PEMKEY=${3}
 
 # create snapshot
 TIMESTAMP=`date '+%Y%m%d-%H%M%S'`
